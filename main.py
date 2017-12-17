f7cacf0da8155bda656e04ebda8e22dd5b36b2cb
ozg <ouzhiganggba@gmail.com>
2017-12-17 16:50:35 +0800

    加上锁

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys, os, threading, time, random, queue
 
 class RunThread(threading.Thread):
     
-	def __init__(self, name, thread_queue = None):
+	def __init__(self, name, thread_mutex, thread_queue = None):
 		threading.Thread.__init__(self)
 		self.name = name
 		
@@ -11,9 +11,15 @@ class RunThread(threading.Thread):
 			self.thread_queue.put(self)
 			print("添加了" + self.name + "线程到列队")
 		
+		self.thread_mutex = thread_mutex
+		
 	def run(self):
+		
 		time.sleep(random.uniform(1, 3))
-		print(self.name + ' 执行完成!')
+		
+		if self.thread_mutex.acquire(10):
+			print(self.name + ' 执行完成!')			
+			self.thread_mutex.release()
 		
 		if self.thread_queue != None:
 			#向任务已经完成的队列发送一个信号
@@ -24,10 +30,12 @@ if __name__ == "__main__":
 	#运行线程数
 	thread_count = 100
 	
+	thread_mutex = threading.Lock()
+	
 	thread_queue = queue.Queue()
 	for i in range(0, thread_count):
-		t = RunThread("thread_" + str(i), thread_queue)
-		t.start()		
+		t = RunThread("thread_" + str(i), thread_mutex, thread_queue)
+		t.start()
 	
 	thread_queue.join()
 	print("全部列队执行完成")
